1a408773fddd1e87195e9d1795778d3755aa047a
NEOPRINTCATAMARCA <fabad401@gmail.com>
2026-09-08 15:06:31 -0300

    Verificar sitio en Google

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,23 @@
-import './globals.css'
-import type { Metadata } from 'next'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Food SaaS',
-  description: 'Sistema SaaS multi-comercio para pedidos gastronómicos'
-}
+  title: "Food SaaS",
+  description: "Sistema SaaS multi-comercio para pedidos gastronómicos",
+  manifest: "/manifest.webmanifest",
+  verification: {
+    google: "SFfeTNDPXXKRF39wnVOaO3WvdQ4CgyPauJ8KtFkBWh8",
+  },
+};
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="es">
       <body>{children}</body>
     </html>
-  )
+  );
 }
