77ec68a76391a1d8dcc6ec654707ab98e960f5d5
NEOPRINTCATAMARCA <fabad401@gmail.com>
2026-09-08 14:19:19 -0300

    Ajustar columnas final

--- app/panel/layout.tsx
+++ app/panel/layout.tsx
@@ -1,23 +1,5 @@
 import type { ReactNode } from "react";
 
 export default function PanelLayout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      <style>{`
-        .bb-panel-fit {
-          width: 100%;
-          min-width: 0;
-        }
-
-        @media (min-width: 1200px) {
-          .bb-panel-fit {
-            zoom: 0.88;
-            width: 113.64%;
-          }
-        }
-      `}</style>
-
-      <div className="bb-panel-fit">{children}</div>
-    </>
-  );
+  return <>{children}</>;
 }
